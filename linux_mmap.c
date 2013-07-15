b68afa44f05b9bc0cb575d3b7a6bf0a8ef2de7af
xuzhenping <xuzhenping@yaxon.com>
2013-07-15 16:06:26 +0800

    at c, jffs2文件系统不支持mmap + MAP_SHEARED

--- linux_mmap.c
+++ linux_mmap.c
@@ -2,6 +2,16 @@
     and then create NRECORDS versions each recording their number.
     These are appended to the file records.dat.  */
 
+
+/*
+    注意：JFFS2文件系统不支持mmap + MAP_SHEARED，而此前我接触的linux嵌入式车台终端的Flash
+就是JFFS2的文件系统。
+
+    linux系统调用mmap在映射一个文件时只有使用MAP_SHEARED参数才会将映射内存的修改写回文件，
+但是jffs2文件系统却不支持有写回的映射，即MAP_SHARED参数不能使用，只能使用MAP_PRIVATE
+进行映射，实践得出，后google得知是jffs2的一大特性，具体原因尚待进一步确认
+*/
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/mman.h>
@@ -50,11 +60,20 @@ int main()
     f = open("records.dat",O_RDWR);
     mapped = (RECORD *)mmap(0, NRECORDS*sizeof(record), 
                           PROT_READ|PROT_WRITE, MAP_SHARED, f, 0);
+    
+    // JFFS2文件系统会返回失败
+    if(MAP_FAILED == mapped)
+    {
+        perror("mmap fail");
+        goto exit;
+    }
 
     mapped[43].integer = 243;
     sprintf(mapped[43].string,"RECORD-%d",mapped[43].integer);
 
     msync((void *)mapped, NRECORDS*sizeof(record), MS_ASYNC);
+    
+exit:
     munmap((void *)mapped, NRECORDS*sizeof(record));
     close(f);
 
