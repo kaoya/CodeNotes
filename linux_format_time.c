c73adc16c1d5a3af2171de0d05af8685cc730325
xuzhenping <xuzhenping@yaxon.com>
2016-09-21 09:50:00 +0800

    格式化标准预定义宏__DATE__和__TIME__，转化为为yyyy-mm-dd HH:MM:SS

--- linux_format_time.c
+++ linux_format_time.c
@@ -3,6 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <assert.h>
+
+/************************************************************************
+**函数：format__DATE__TIME__
+**功能：格式化标准预定义宏__DATE__和__TIME__，转化为yyyy-mm-dd HH:MM:SS格式
+**参数：无
+**返回：none
+**作者：KaoYa, 2016-09-21
+**备注：
+    1). __DATE__的格式为（举例）：Sep 21 2016
+        __TIME__的格式为（举例）：09:56:54
+************************************************************************/
+void format__DATE__TIME__(void)
+{
+    char *ret;
+    char des[50];
+    struct tm tm;
+    static const char *src = __DATE__" "__TIME__;
+
+    memset(&tm, 0, sizeof(struct tm));
+    ret = strptime(src, "%b %d %Y %H:%M:%S", &tm);
+    assert(ret);
+    
+    strftime(des, sizeof(des), "%G-%m-%d %H:%M:%S", &tm);
+    printf("[%s] >>> [%s]\n", src, des);
+}
 
 int main(void)
 {
@@ -22,6 +48,8 @@ int main(void)
     
     strftime(buf, sizeof(buf), "%G-%m-%d %H:%M:%S", &tm);
     puts(buf);
+    
+    format__DATE__TIME__();
     
     exit(EXIT_SUCCESS);
 }
