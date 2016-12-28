3e138089354aa00a307c5d1dd7120e6a4d5c596d
xuzhenping <xuzhenping@yaxon.com>
2016-12-28 14:19:58 +0800

    Modified   windows_linux_format_input_output.c

--- windows_linux_format_input_output.c
+++ windows_linux_format_input_output.c
@@ -8,11 +8,13 @@
 -------------------------------------------------------------------------
         %10s         "Hello"              |     Hello|
         %-10s        "Hello"              |Hello     |
+        %*s          10,"Hello"           |     Hello|
+        %*s          3,"Hello"            |Hello|
+        %.*s         3,"Hello"            |Hel|
         %10d         1234                 |      1234|
         %-10d        1234                 |1234      |
         %010d        1234                 |0000001234|
         %10.4f       12.34                |   12.3400|
-        %*s          10,"Hello"           |     Hello|
  -------------------------------------------------------------------------
 */
 static void format_input_output(void)
@@ -20,6 +22,8 @@ static void format_input_output(void)
 	printf("%10s\n",    "Hello");
 	printf("%-10s\n",   "Hello");
 	printf("%*s\n", 10, "Hello");
+	printf("%*s\n", 3,  "Hello");
+	printf("%.*s\n", 3, "Hello");
 	
 	printf("%10d\n",  1234);
 	printf("%-10d\n", 1234);
